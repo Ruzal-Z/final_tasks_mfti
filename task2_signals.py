7e9282618e13441462042b20df1fab139bca6f76
Ruzal <labrhub@gmail.com>
2022-09-22 22:07:46 +0300

    add task2

---
+++ task2_signals.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def fun_signal(f):
+    arr = np.array(f)
+    plt.plot(arr)
+
+    arr1 = np.zeros(len(arr))
+    for i in range(len(arr)):
+        if i < 10:
+            arr1[i] = sum(arr[0: i + 1]) / (i + 1)
+        else:
+            arr1[i] = sum(arr[i - 9: i + 1]) / 10
+
+    plt.plot(arr1)
+    plt.show()
+
+fun_signal(f = np.loadtxt('signals/signal01.dat', float))
+fun_signal(f = np.loadtxt('signals/signal02.dat', float))
+fun_signal(f = np.loadtxt('signals/signal03.dat', float))
