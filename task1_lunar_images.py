69efabb448e062673261a96a629840b3cffa27d4
Ruzal <labrhub@gmail.com>
2022-09-26 22:22:58 +0300

    update

--- task1_lunar_images.py
+++ task1_lunar_images.py
@@ -3,9 +3,22 @@ import numpy as np
 
 img = Image.open('lunar_images/lunar01_raw.jpg')
 data = np.array(img)
-
-ar = [(i - np.min(data)) * (255 //((np.max(data))-(np.min(data)))) for i in data]
-
+ar = [(i - np.min(data)) * (255 // ((np.max(data))-(np.min(data)))) for i in data]
 updated_data = np.array(ar)
 res_img = Image.fromarray(updated_data)
 res_img.save('lunar_images/new_lunar01_raw.jpg')
+
+img = Image.open('lunar_images/lunar02_raw.jpg')
+data = np.array(img)
+ar = [(i - np.min(data)) * (255 // ((np.max(data))-(np.min(data)))) for i in data]
+updated_data = np.array(ar)
+res_img = Image.fromarray(updated_data)
+res_img.save('lunar_images/new_lunar02_raw.jpg')
+
+img = Image.open('lunar_images/lunar03_raw.jpg')
+data = np.array(img)
+ar = [(i - np.min(data)) * (255 // ((np.max(data))-(np.min(data)))) for i in data]
+updated_data = np.array(ar)
+res_img = Image.fromarray(updated_data)
+res_img.save('lunar_images/new_lunar03_raw.jpg')
+    
