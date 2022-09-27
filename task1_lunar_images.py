d9be0f29bd2fc7c8cd3c0783994d38d6dbe271fa
Ruzal <labrhub@gmail.com>
2022-09-27 13:58:49 +0300

    add func task1

--- task1_lunar_images.py
+++ task1_lunar_images.py
@@ -1,27 +1,17 @@
 from PIL import Image # считаем картинку в numpy array
 import numpy as np
 
-img = Image.open('lunar_images/lunar01_raw.jpg')
-data1 = np.array(img)
-img = Image.open('lunar_images/lunar02_raw.jpg')
-data2 = np.array(img)
-img = Image.open('lunar_images/lunar03_raw.jpg')
-data3 = np.array(img)
+def update_img(f_name):
+    img = Image.open(f_name)
+    data = np.array(img)
+    updated_data = [(i - np.min(data)) * (255 // ((np.max(data)) - (np.min(data)))) for i in data]
+    updated_data = np.array(updated_data )
+    res_img = Image.fromarray(updated_data)
+    res_img.save(f'lunar_images/new_lunar{f_name[-10:-8]}_raw.jpg')
+    res_img.show()
 
-ar1 = [(i - np.min(data1)) * (255 // ((np.max(data1))-(np.min(data1)))) for i in data1]
-ar2 = [(i - np.min(data2)) * (255 // ((np.max(data2))-(np.min(data2)))) for i in data2]
-ar3 = [(i - np.min(data3)) * (255 // ((np.max(data3))-(np.min(data3)))) for i in data3]
+update_img('lunar_images/lunar01_raw.jpg')
+update_img('lunar_images/lunar02_raw.jpg')
+update_img('lunar_images/lunar03_raw.jpg')
 
 
-updated_data = np.array(ar1)
-res_img = Image.fromarray(updated_data)
-res_img.save('lunar_images/new_lunar01_raw.jpg')
-
-updated_data = np.array(ar2)
-res_img = Image.fromarray(updated_data)
-res_img.save('lunar_images/new_lunar02_raw.jpg')
-
-updated_data = np.array(ar3)
-res_img = Image.fromarray(updated_data)
-res_img.save('lunar_images/new_lunar03_raw.jpg')
-    
